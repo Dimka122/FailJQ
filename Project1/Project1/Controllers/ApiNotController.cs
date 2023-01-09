53360891210f243fa2be7859c5c9199ac3d12dd3
Дмитрий Кассин <kassindima1@gmail.com>
2023-01-09 20:15:17 +0200

    ///

---
+++ Project1/Project1/Controllers/ApiNotController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Notes.Controllers
+{
+    public class ApiNotController : Controller
+    {
+        List<Not> nots = new List<Not>
+
+{
+            new() { Title = "Tom", Content = "hgfhjhfkjg", Token_id = "1234567890"},
+            new() { Title = "Bob", Content = "Bom", Token_id = "Tobik" },
+            new() { Title = "Sam", Content = "Sam", Token_id = "Sam" },
+            new() { Title = "Tom", Content = "Tom", Token_id = "Tom" },
+        };
+
+        [HttpGet]
+        public IEnumerable<Not> GetNot()
+        {
+            IEnumerable<Not> nots = null;
+            using (IDbConnection db = new SqlConnection("Data Source = (localdb)/MSSQLLocalDB; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
+            {
+                nots = db.GetAll<Not>();
+            }
+            return nots;
+        }
+        [HttpGet("{apiNotId}")]
+        public Not? GetNotById(int id)
+        {
+            Not? not = null;
+            using (IDbConnection db = new SqlConnection("Data Source = (localdb)/MSSQLLocalDB; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False"))
+            {
+                not = db.Get<Not>(id);
+            }
+            return not;
+
+        }
+
+
+    }
+}
+
---
+++ Project1/Project1/Controllers/Not.cs
@@ -0,0 +1,11 @@
+﻿namespace Notes.Controllers
+{
+    internal class Not
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Token_id { get; set; }
+        public List<Not> Nots { get; set; }
+}
+}
